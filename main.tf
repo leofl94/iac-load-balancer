80a9b7c50847a361e5de92187e003c5dcccc2dea
Leonardo Ferreira de Lima <leofl94@gmail.com>
2022-09-23 23:05:40 -0300

    Alterando main.tf

--- main.tf
+++ main.tf
@@ -1,71 +1,48 @@
+terraform {
+  backend "gcs" {
+    bucket  = "mentoria-tfstate-staging"
+    prefix  = "iac-load-balancer/state"
+  }
+}
+
 provider "google" {
-  project = "mentoriaiac-20220916"
+  project = "mentoria-iac-staging"
   region  = "us-central1"
 }
 
-locals {
-  subnets = [data.google_compute_subnetwork.load-balance, data.google_compute_subnetwork.nomad]
+data "google_compute_network" "orquestradores" {
+  name = "orquestradores"
 }
 
-data "google_compute_network" "vpc-network" {
-  name = "vpc-network"
+data "google_compute_subnetwork" "load-balancer" {
+  name = "load-balancer"
 }
 
-data "google_compute_subnetwork" "load-balance" {
-  name = "load-balance"
-}
-
-data "google_compute_subnetwork" "nomad" {
-  name = "nomad"
-}
-
-resource "google_compute_firewall" "load-balance" {
-  name          = "load-balance"
-  network       = data.google_compute_network.vpc-network.name
+resource "google_compute_firewall" "load-balancer" {
+  name          = "load-balancer"
+  network       = data.google_compute_network.orquestradores.name
   source_ranges = ["0.0.0.0/0"]
-  // destination_ranges = [data.google_compute_subnetwork.load-balance.ip_cidr_range]
-  target_tags = ["load-balance"]
+  target_tags = ["load-balancer"]
   allow {
     protocol = "tcp"
-    ports    = ["80", "22"]
+    ports    = ["80", "22", "443", "4646"]
   }
-
 }
 
 module "compute_gcp" {
   source         = "github.com/mentoriaiac/iac-modulo-compute-gcp"
-  project        = "mentoriaiac-20220916"
-  instance_name  = "nginx-0"
-  instance_image = "nginx-20220917004516"
+  count          = 2
+  project        = "mentoria-iac-staging"
+  instance_name  = "nginx-${count.index}"
+  instance_image = "nginx-20220924001409"
   machine_type   = "e2-small"
   zone           = "us-central1-a"
-  network        = data.google_compute_network.vpc-network.name
-  subnetwork     = local.subnets[0].name
-
-
-  labels = {
-    value = "key"
-  }
-
-  tags = ["load-balance"]
-
-}
-
-module "nomad" {
-  source         = "github.com/mentoriaiac/iac-modulo-compute-gcp"
-  project        = "mentoriaiac-20220916"
-  instance_name  = "nginx-1"
-  instance_image = "nginx-20220917004516"
-  machine_type   = "e2-small"
-  zone           = "us-central1-a"
-  network        = data.google_compute_network.vpc-network.name
-  subnetwork     = local.subnets[1].name
+  network        = data.google_compute_network.orquestradores.name
+  subnetwork     = data.google_compute_subnetwork.load-balancer.name
   public_ip      = "ephemeral"
+
   labels = {
     value = "key"
   }
-
-
-  tags = []
-
+  tags = ["load-balancer"]
 }
